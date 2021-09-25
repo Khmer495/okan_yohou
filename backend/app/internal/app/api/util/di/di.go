405c733e3d8b980408c107848fcd1eacd86f4ed9
久米 祐貴 <kume_yuki@cyberagent.co.jp>
2021-09-25 23:34:39 +0900

    add di

--- backend/app/internal/app/api/util/di/di.go
+++ backend/app/internal/app/api/util/di/di.go
@@ -1,6 +1,10 @@
 package di
 
 import (
+	"github.com/Khmer495/okan_yohou/internal/app/api/handler"
+	"github.com/Khmer495/okan_yohou/internal/app/api/infrastructure/echo"
+	"github.com/Khmer495/okan_yohou/internal/pkg/domain/repository"
+	"github.com/Khmer495/okan_yohou/internal/pkg/domain/usecase"
 	"go.uber.org/dig"
 	"go.uber.org/zap"
 )
@@ -9,33 +13,35 @@ func newPanic(err error) {
 	zap.S().Panicf("di: %+v", err)
 }
 
-func provideService(d *dig.Container) {
-
-}
-
 func provideRepository(d *dig.Container) {
-
+	if err := d.Provide(repository.NewAlertRepository); err != nil {
+		newPanic(err)
+	}
 }
 
 func provideUsecase(d *dig.Container) {
-
+	if err := d.Provide(usecase.NewAlertUsecase); err != nil {
+		newPanic(err)
+	}
 }
 
 func provideHandler(d *dig.Container) {
-
+	if err := d.Provide(handler.NewHandler); err != nil {
+		newPanic(err)
+	}
 }
 
 func invokeEchoServer(d *dig.Container) {
-	// if err := d.Invoke(echo.NewEchoServer); err != nil {
-	// 	newPanic(err)
-	// }
+	if err := d.Invoke(echo.NewEchoServer); err != nil {
+		newPanic(err)
+	}
 }
 
 func NewDig() *dig.Container {
 	d := dig.New()
-	provideService(d)
 	provideRepository(d)
 	provideUsecase(d)
+	provideHandler(d)
 	invokeEchoServer(d)
 	return d
 }
